5a40beb53edbbf17d819557ff6fb261462a0bf60
SPIGS <paspielvogel@gmail.com>
2020-04-14 14:47:46 -0400

    Removed old files

--- dicebot.py
+++ dicebot.py
@@ -1,45 +1,55 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os.path
+from os import path
+from gamemode import GameMode
 
-extensions = ["cogs.characters", "cogs.dice", "cogs.equipment", "cogs.spells"]
+token = ""
+command_prefix = ""
 
-bot = commands.Bot(command_prefix='.')
+extensions = ["cogs.roll", "cogs.reference", "cogs.utility"]
+
+def load_user_info():
+    global token
+    global command_prefix
+    with open("docs/user.info", 'r') as user_info:
+        lines = user_info.read().splitlines()
+        token = lines[0]
+        command_prefix = lines[1]
+
+bot = commands.Bot(command_prefix="!") 
 
 if __name__ == '__main__':
+    if path.exists("docs/user.info"):
+        print("User info exists; starting bot...")
+        load_user_info()
+    else:
+        print("User info not found; prompting for info...")
+        token = input("Enter your bot's token: ")
+        command_prefix = input("Enter your bot's command prefix: ")
+        
+        with open("docs/user.info", 'w') as user_file:
+            user_file.write(token + "\n")
+            user_file.write(command_prefix)
+        
+        load_user_info()
+    
+    bot.command_prefix = command_prefix
+    bot.remove_command("help")
     for extension in extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print('failed to load extension')
+        bot.load_extension(extension)
+    bot.current_gamemode = GameMode.WIZARDS_FIFTH_ED
+
 
 @bot.event
 async def on_ready():
-    print ("Let's Roll!")
-    print (bot.user.name)
-    print (bot.user.id)
-    await bot.change_presence(game=discord.Game(name='5e | !help'))
-
-@bot.command(pass_context=True, description="Clears the text chat; for use by channel administators only.", aliases=["clr","cls"])
-async def clear(ctx):
-    if canClearChat(ctx.message):
-        async for msg in bot.logs_from(ctx.message.channel):
-            await bot.delete_message(msg)
-    else:
-        await bot.say("You do not have permissions to do that.")
-
-def canClearChat(message):
-    author = message.author
-    rolenames = []
-    for role in author.roles:
-        rolenames.append(role.name)
-
-    if "Owner" in rolenames:
-        return True
-    elif "Admin" in rolenames:
-        return True
-    else:
-        return False
+    print("Let's Roll!")
+    print(bot.user.name)
+    print(bot.user.id)
+    await bot.change_presence(activity=discord.Game(name='D&D 5th Edition | ' + bot.command_prefix + 'help'))
+    
+bot.run(token, bot=True, reconnect=True)
 
 
-bot.run("insert your token here")
+
